fbbf7b465f3edc59dd800924612d5275044798c1
Jose Argueta <jajose780@gmail.com>
2021-09-16 22:23:02 -0700

    header skeleton functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,9 +7,9 @@ import Footer from "./components/Footer"
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Body/>
-      <Footer/>
+      <Header />
+      <Body />
+      <Footer />
     </div>
   )
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import "./Header.css";
+import React from "react"
+import "./Header.css"
+import AccountCircleIcon from "@mui/icons-material/AccountCircle"
+import AdjustIcon from "@mui/icons-material/Adjust"
+import InsertCommentIcon from "@mui/icons-material/InsertComment"
+import IconButton from "@mui/material/IconButton"
 
 function header() {
-    return (
-        <div className="header">
-            <h2>hello world</h2>
-        </div>
-    )
+  return (
+    <div className="header">
+      <IconButton>
+        <AccountCircleIcon className="profile_icon" fontSize="large" />
+      </IconButton>
+      <IconButton>
+        <AdjustIcon className="placeHolder_icon" fontSize="large" />
+      </IconButton>
+      <IconButton>
+        <InsertCommentIcon className="directMessge_icon" fontSize="large" />
+      </IconButton>
+    </div>
+  )
 }
 
 export default header
