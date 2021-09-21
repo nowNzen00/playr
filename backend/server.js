5aa91e209ed7c67af84b8420654c48b31e97978b
Jose Argueta <jajose780@gmail.com>
2021-09-21 09:09:04 -0700

    backend update/bug fix

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,44 @@
-import express from "express";
-import mongoose from "mongoose";
-import Cards from "./dbCards.js";
+import express from "express"
+import mongoose from "mongoose"
+import Cards from "./dbCards.js"
 
 // App Config
-const app = express();
-const port = process.env.PORT || 8001;
+const app = express()
+const port = process.env.PORT || 8001
 
-const connection_url = `mongodb+srv://admin:xzvCOUMA12fH9df3@cluster0.nzh5r.mongodb.net/playrdb?retryWrites=true&w=majority`;
+const connection_url = `mongodb+srv://admin:xzvCOUMA12fH9df3@cluster0.nzh5r.mongodb.net/playrdb?retryWrites=true&w=majority`
 
 //Middleware
 
 //DB config
 mongoose.connect(connection_url, {
   useNewUrlParser: true,
-  useCreateIndex: true,
   useUnifiedTopology: true,
-});
+})
 
 //API Endpoints
-app.get("/", (req, res) => res.status(200).send("success"));
+app.get("/", (req, res) => res.status(200).send("success"))
 app.post("/playr/cards", (req, res) => {
-  const dbCard = req.body;
+  const dbCard = req.body
 
   Cards.create(dbCard, (err, data) => {
     if (err) {
-      res.status(500).send(err);
+      res.status(500).send(err)
     } else {
-      res.status(201).send(data);
+      res.status(201).send(data)
     }
-  });
-});
+  })
+})
 
 app.get("/playr/cards"),
   (req, res) => {
-    Cards.create(dbCard, (err, data));
+    Cards.create(dbCard, (err, data))
     if (err) {
-      res.status(500).send(err);
+      res.status(500).send(err)
     } else {
-      res.status(201).send(data);
+      res.status(201).send(data)
     }
-  };
+  }
 
 //Listener
-app.listen(port, () => console.log(`listening on localhost: ${port})`));
+app.listen(port, () => console.log(`listening on localhost: ${port}`))
