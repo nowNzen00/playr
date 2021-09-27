8c3699e893bd76ac73b7960581c8cb3a6fc3b958
Jose Argueta <jajose780@gmail.com>
2021-09-27 01:18:56 -0700

    profile card created

--- frontend/src/components/ProfilePage.js
+++ frontend/src/components/ProfilePage.js
@@ -1,10 +1,15 @@
 import React from "react";
+import "./ProfilePage.css";
 
 function ProfilePage() {
   return (
-    <div>
-      <h1> Profile Page</h1>
-    </div>
+    <body>
+      <div className="profile-container">
+        <div className="profile-menu">
+          <h1>Profile</h1>
+        </div>
+      </div>
+    </body>
   );
 }
 
