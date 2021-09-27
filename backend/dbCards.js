25ce853ba5dd495148e1efbbca525d081379ee14
Jose Argueta <jajose780@gmail.com>
2021-09-27 10:46:33 -0700

    database cards schema update

--- backend/dbCards.js
+++ backend/dbCards.js
@@ -1,10 +1,11 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
 const cardSchema = mongoose.Schema({
   name: String,
   imgUrl: String,
   description: String,
   interest: String,
-});
+  matches: String,
+})
 
-export default mongoose.model("cards", cardSchema);
+export default mongoose.model("cards", cardSchema)
