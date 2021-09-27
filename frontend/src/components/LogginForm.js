da8a2a7ff03889b33e557905791ad16d4308b3fb
Jose Argueta <jajose780@gmail.com>
2021-09-26 23:15:11 -0700

    login page w/ bug

--- frontend/src/components/LogginForm.js
+++ frontend/src/components/LogginForm.js
@@ -1,7 +1,46 @@
-import React from "react"
+import React from "react";
+import "./LoginForm.css";
+import PersonIcon from "@mui/icons-material/Person";
+import LockIcon from "@mui/icons-material/Lock";
+import { BrowserRouter as Link } from "react-router-dom";
 
 function LogginForm() {
-  return <div></div>
+  return (
+    <body>
+      <div className="login-container">
+        <div className="box">
+          <h1>Log in</h1>
+          <div className="user">
+            <PersonIcon className="icon" />
+            <input
+              type="text"
+              name="username"
+              id="username"
+              autocomplete="off"
+              placeholder="Username"
+            />
+            <LockIcon className="icon" />
+            <input
+              type="text"
+              name="password"
+              id="password"
+              autocomplete="off"
+              placeholder="Password"
+            />
+          </div>
+          <p className="reset-password">Forgot Password?</p>
+          <div className="login-button">
+            <Link to="/">
+              <button className="button">Log in</button>
+            </Link>
+            <p className="signup">
+              First time here? <span>Sign Up</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </body>
+  );
 }
 
-export default LogginForm
+export default LogginForm;
