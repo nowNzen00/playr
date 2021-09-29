27c9cac939fa9f16c11c790c852496fe0a8673ee
Jose Argueta <jajose780@gmail.com>
2021-09-28 23:16:31 -0700

    construction notice in direct messaging component

--- frontend/src/components/DirectMessagePage.js
+++ frontend/src/components/DirectMessagePage.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "./DirectMessagePage.css";
+import { Link } from "react-router-dom";
+import ConstructionIcon from "@mui/icons-material/Construction";
 
 function DirectMessagePage() {
   return (
-    <div>
-      <h1>Direct Message</h1>
+    <div className="under-construction">
+      <h1>404</h1>
+      <div className="icon-box">
+        <ConstructionIcon className="hammer" />
+      </div>
+      <h3>I'm sorry, it looks like this area is under construction</h3>
+      <Link to="/">
+        <div className="home-button">
+          <button className="construction-button">Home</button>
+        </div>
+      </Link>
     </div>
   );
 }
